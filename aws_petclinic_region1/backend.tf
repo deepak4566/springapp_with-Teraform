e9910083b55a51754c1321cca1858b6b69267b42
Deepak Reddy <deepakreddyats1@gmail.com>
2025-06-09 21:38:56 +0530

    Delete aws_petclinic_region1/backend.tf

--- aws_petclinic_region1/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-   backend "s3" {
-    bucket         = "deepak-s3-bucket1" # change this
-    key            = "deepak/terraform.tfstate"
-    region         = "ap-south-1"
-    encrypt        = true
-    dynamodb_table = "terraform-lock"
- }
- }
