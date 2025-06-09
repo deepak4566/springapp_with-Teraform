38936bc2d2070ad183e7b108bc8bb71e7d2fa47c
Deepak Reddy <deepakreddyats1@gmail.com>
2025-06-09 21:22:10 +0530

    Delete main directory

--- main/statefile.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "vegeta-terraform-remote-state"
-    key     = "infra.tfstate"
-    region  = "eu-west-2"
-    profile = "default"
-    dynamodb_table = "vegeta-terraform-remote-state-table"
-  }
-}
