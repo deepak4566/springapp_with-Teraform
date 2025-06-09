4b05d99a92e14dabfd690b1597ef61d75e97f54e
Deepak Reddy <deepakreddyats1@gmail.com>
2025-06-09 21:22:19 +0530

    Delete modules directory

--- modules/rds/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "rds_db_endpoint" {
-    value = aws_db_instance.db_instance.endpoint
-}
--- modules/security_group/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "alb_security_group_id" {
-    value = aws_security_group.alb_security_group.id
-}
